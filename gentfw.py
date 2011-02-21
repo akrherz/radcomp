77f54a0788c92405dc6b98a90baf4183c50b9af0
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2011-02-21 19:25:17 +0000

    fix what is inserted into the DB

--- gentfw.py
+++ gentfw.py
@@ -11,6 +11,7 @@ ts = mx.DateTime.strptime(sts, '%Y%m%d%H%M')
 out = open("%s_N0Q_CLEAN_%s.tfw" % (sector, v), 'w')
 
 if sector == 'US':
+    wkt = "-126 50,-66 50,-66 24,-126 24,-126 50"
     out.write("""   0.0050000000000%s
    0.00000
    0.00000
@@ -18,6 +19,7 @@ if sector == 'US':
 -126.000000
   50.0000""" % (v, random.randint(0,1000) ) )
 elif sector == 'AK':
+    wkt = "-170.5 68.71,-130.50 68.71,-130.50 53.21,-170.5 53.21,-170.5 68.71"
     out.write("""   0.010000000000%s
    0.00000
    0.00000
@@ -26,6 +28,7 @@ elif sector == 'AK':
   68.7100""" % (v, random.randint(0,1000) ) )
     
 elif sector == 'HI': 
+    wkt = "-162.41 24.44,-152.41 24.44,-152.41 15.44,-162.41 15.44,-162.41 24.44"
     out.write("""   0.005000000000%s
    0.00000
    0.00000
@@ -38,7 +41,7 @@ out.close()
 mydb = pg.connect('postgis', 'iemdb')
 mydb.query("SET TIME ZONE 'GMT'")
 sql = """INSERT into nexrad_n0q_tindex( the_geom, datetime, filepath) values 
-  ('SRID=4326;MULTIPOLYGON(((-126 50,-66 50,-66 24,-126 24,-126 50)))', '%s', 
-  '/mesonet/ARCHIVE/data/%s/GIS/%scomp/n0q_%s.png')""" % (ts.strftime("%Y-%m-%d %H:%M"), 
+  ('SRID=4326;MULTIPOLYGON(((%s)))', '%s', 
+  '/mesonet/ARCHIVE/data/%s/GIS/%scomp/n0q_%s.png')""" % (wkt, ts.strftime("%Y-%m-%d %H:%M"), 
   ts.strftime("%Y/%m/%d"), sector.lower(), ts.strftime("%Y%m%d%H%M") )
 mydb.query(sql)
