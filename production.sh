57e04ad1480e6b20eddb43f60f7c3073e72146a1
akrherz <akrherz@iastate.edu>
2012-10-25 07:11:18 -0500

    support archived reruns

--- production.sh
+++ production.sh
@@ -2,11 +2,16 @@
 # $1 is the sector
 # $2 is the minutes offset
 
-export YYYY=$(date --date "$2 minute" -u +%Y)
-export MM=$(date --date "$2 minute" -u +%m)
-export DD=$(date --date "$2 minute" -u +%d)
-export HH=$(date --date "$2 minute" -u +%H)
-export MI=$(date --date "$2 minute" -u +%M)
+export YYYY=$2
+export MM=$3
+export DD=$4
+export HH=$5
+export MI=$6
+if [ $7 = "RT" ]; then
+	export routes="acr"
+else
+	export routes="a"
+fi
 
 # Our Job ID will be $$
 touch ${1}_N0Q_LOCK_$$
@@ -26,7 +31,7 @@ done
 /usr/local/python/bin/python process.py $HH $$ $1
 
 # Lets insert it into LDM
-/home/ldm/bin/pqinsert -p "gis acr ${YYYY}${MM}${DD}${HH}${MI} gis/images/4326/${1}COMP/n0q_ GIS/${1,,}comp/n0q_${YYYY}${MM}${DD}${HH}${MI}.png png" ${1}_N0Q_CLEAN_$$.png
+/home/ldm/bin/pqinsert -p "gis $routes ${YYYY}${MM}${DD}${HH}${MI} gis/images/4326/${1}COMP/n0q_ GIS/${1,,}comp/n0q_${YYYY}${MM}${DD}${HH}${MI}.png png" ${1}_N0Q_CLEAN_$$.png
 
 # Do TFW
 python gentfw.py $$ $1 ${YYYY}${MM}${DD}${HH}${MI}
@@ -41,12 +46,14 @@ gdalwarp  -q -s_srs EPSG:4326 -t_srs '+proj=merc +a=6378137 +b=6378137 +lat_ts=0
 
 # Compress, insert
 gzip -c google_${1}_N0Q_CLEAN_$$.tif > google_${1}_N0Q_CLEAN_$$.tif.Z
-/home/ldm/bin/pqinsert -p "gis r ${YYYY}${MM}${DD}${HH}${MI} gis/images/900913/${1}COMP/n0q_ bogus tif.Z" google_${1}_N0Q_CLEAN_$$.tif.Z
-  
+if [ $7 = "RT" ]; then
+	/home/ldm/bin/pqinsert -p "gis r ${YYYY}${MM}${DD}${HH}${MI} gis/images/900913/${1}COMP/n0q_ bogus tif.Z" google_${1}_N0Q_CLEAN_$$.tif.Z
+fi
 # Compress, insert
 gzip -c ${1}_N0Q_CLEAN_$$.tif > ${1}_N0Q_CLEAN_$$.tif.Z
-/home/ldm/bin/pqinsert -p "gis r ${YYYY}${MM}${DD}${HH}${MI} gis/images/4326/${1}COMP/n0q_ bogus tif.Z" ${1}_N0Q_CLEAN_$$.tif.Z
-
+if [ $7 = "RT" ]; then
+	/home/ldm/bin/pqinsert -p "gis r ${YYYY}${MM}${DD}${HH}${MI} gis/images/4326/${1}COMP/n0q_ bogus tif.Z" ${1}_N0Q_CLEAN_$$.tif.Z
+fi
 
 # Cleanup
 rm -f ${1}_N0Q_CLEAN_$$.png ${1}_N0Q_$$.gif ${1}_NET_$$.gif ${1}_N0Q_CLEAN_$$.tfw 
