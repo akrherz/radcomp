bdcc08323465eb2cc71af47fd3e122c2c1290e8d
akrherz <akrherz@iastate.edu>
2014-07-16 12:21:40 -0500

    s/NET/EET/

--- production.sh
+++ production.sh
@@ -56,8 +56,8 @@ if [ $7 = "RT" ]; then
 fi
 
 # Cleanup
-rm -f ${1}_N0Q_CLEAN_$$.png ${1}_N0Q_$$.gif ${1}_NET_$$.gif ${1}_N0Q_CLEAN_$$.tfw 
+rm -f ${1}_N0Q_CLEAN_$$.png ${1}_N0Q_$$.gif ${1}_EET_$$.gif ${1}_N0Q_CLEAN_$$.tfw 
 rm -f ${1}_N0Q_CLEAN_$$.tif.Z ${1}_N0Q_CLEAN_$$.tif google_${1}_N0Q_CLEAN_$$.tif.Z google_${1}_N0Q_CLEAN_$$.tif
 
 python scripts/create_metadata.py $1 $YYYY $MM $DD $HH $MI N0Q
-python scripts/create_metadata.py $1 $YYYY $MM $DD $HH $MI NET
+python scripts/create_metadata.py $1 $YYYY $MM $DD $HH $MI EET
