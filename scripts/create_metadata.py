793b0fe4c7102109abef26abfe89adb6a16b40e1
akrherz <akrherz@iastate.edu>
2014-08-08 12:43:02 -0500

    add some debug printing to nex2img to add metadata to JSON output regarding number of radars present

--- scripts/create_metadata.py
+++ scripts/create_metadata.py
@@ -18,8 +18,20 @@ if __name__ == '__main__':
                             int(sys.argv[6]) )
     prod = sys.argv[7]
     
+    radars = 0
+    used = 0
+    logfn = "logs/nex2img_%s_%s.log" % (sector, prod)
+    if os.path.isfile(logfn):
+        for line in open(logfn):
+            if line.find("Searching radar:") > 0:
+                radars += 1
+            elif line.find("Using image:") > 0:
+                used += 1
+        
+    
     res = {'meta': {'vcp': None, 'product': prod, 'site': '%sCOMP' % (sector,),
-                    'valid': ts.strftime("%Y-%m-%dT%H:%M:%SZ")}}
+                    'valid': ts.strftime("%Y-%m-%dT%H:%M:%SZ"),
+                    'radar_quorum': "%s/%s" % (used, radars)}}
     
     (tmpfp, tmpfn) = tempfile.mkstemp()
     os.write(tmpfp, json.dumps(res) )
