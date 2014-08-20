60f2a0aa73c61ca4a4e69b0a0c89005d83c85fd7
akrherz <akrherz@iastate.edu>
2014-08-20 07:49:53 -0500

    cleanup and exec n0r.csh with csh

--- rerunhour.py
+++ rerunhour.py
@@ -1,20 +1,26 @@
-#!/usr/bin/python
+"""
+ This script checks our processing of N0R composites and reruns any missing
+ composites 
+"""
 
-import mx.DateTime
+import datetime
 import os
+import subprocess
 
-now = mx.DateTime.gmt()
-sts = now - mx.DateTime.RelativeDateTime(hours=6,minute=0)
-ets = sts + mx.DateTime.RelativeDateTime(hours=1)
-interval = mx.DateTime.RelativeDateTime(minutes=5)
+sts = datetime.datetime.utcnow()
+sts -= datetime.timedelta(hours=6)
+sts = sts.replace(minute=0,second=0,microsecond=0)
+ets = sts + datetime.timedelta(hours=1)
+interval = datetime.timedelta(minutes=5)
 
 
 now = sts
-while (now < ets):
-  fp = now.strftime("/mesonet/ARCHIVE/data/%Y/%m/%d/GIS/uscomp/n0r_%Y%m%d%H%M.png")
-  if not os.path.isfile(fp):
-    print 'Reprocess n0r:', fp
-    cmd = "./n0r.csh %s n0r 1" % (now.strftime("%Y %m %d %H %M"),)
-    os.system(cmd)
-  now += interval
+while now < ets:
+    fn = now.strftime(("/mesonet/ARCHIVE/data/%Y/%m/%d/"
+                       +"GIS/uscomp/n0r_%Y%m%d%H%M.png"))
+    if not os.path.isfile(fn):
+        print 'Reprocess n0r:', fn
+        cmd = "csh n0r.csh %s n0r 1" % (now.strftime("%Y %m %d %H %M"),)
+        subprocess.call(cmd, shell=True)
+    now += interval
 
