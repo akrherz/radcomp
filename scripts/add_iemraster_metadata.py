4430b5ba6f209db3f57b7a3bf93316caf7a70e38
akrherz <akrherz@iastate.edu>
2014-12-19 15:11:30 -0600

    completed for N0Q

--- scripts/add_iemraster_metadata.py
+++ scripts/add_iemraster_metadata.py
@@ -4,11 +4,11 @@ import psycopg2
 pgconn = psycopg2.connect(database='mesosite')
 cursor = pgconn.cursor()
 
-g = gdal.Open('n0r_201412192055.png')
+g = gdal.Open('n0q_201412192055.png')
 
 ct = g.GetRasterBand(1).GetColorTable()
 
-db = -35
+db = -32
 for i in range(ct.GetCount()):
     (r,g,b,a) = ct.GetColorEntry(i)
     print db, r, g, b
@@ -17,9 +17,9 @@ for i in range(ct.GetCount()):
         r,g,b = [0,0,0]
     cursor.execute("""INSERT into iemrasters_lookup(
         iemraster_id, coloridx, value, r, g, b) VALUES (%s,%s,%s,%s,%s,%s)
-        """, (1, i, db, r, g, b))
-    db += 5
-    if db > 70:
+        """, (2, i, db, r, g, b))
+    db += 0.5
+    if db > 95:
         break
 
 cursor.close()
