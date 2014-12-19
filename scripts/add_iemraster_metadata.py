3fe69d0cd47aa56b8748a536b48d792007761734
akrherz <akrherz@iastate.edu>
2014-12-19 15:09:38 -0600

    Add N0R to iemraster metadata

---
+++ scripts/add_iemraster_metadata.py
@@ -0,0 +1,28 @@
+
+import osgeo.gdal as gdal
+import psycopg2
+pgconn = psycopg2.connect(database='mesosite')
+cursor = pgconn.cursor()
+
+g = gdal.Open('n0r_201412192055.png')
+
+ct = g.GetRasterBand(1).GetColorTable()
+
+db = -35
+for i in range(ct.GetCount()):
+    (r,g,b,a) = ct.GetColorEntry(i)
+    print db, r, g, b
+    if max([r,g,b]) >= 256 or min([r,g,b]) < 0:
+        print 'Invalid Color, assigning black!'
+        r,g,b = [0,0,0]
+    cursor.execute("""INSERT into iemrasters_lookup(
+        iemraster_id, coloridx, value, r, g, b) VALUES (%s,%s,%s,%s,%s,%s)
+        """, (1, i, db, r, g, b))
+    db += 5
+    if db > 70:
+        break
+
+cursor.close()
+pgconn.commit()
+pgconn.close()
+    
