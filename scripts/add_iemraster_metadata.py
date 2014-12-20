b16c0aa354bc762e495ca0128205fada3c375295
akrherz <akrherz@iastate.edu>
2014-12-19 22:38:55 -0600

    finalize MRMS A2M

--- scripts/add_iemraster_metadata.py
+++ scripts/add_iemraster_metadata.py
@@ -4,23 +4,27 @@ import psycopg2
 pgconn = psycopg2.connect(database='mesosite')
 cursor = pgconn.cursor()
 
-g = gdal.Open('n0q_201412192055.png')
+g = gdal.Open('a2m.png')
 
 ct = g.GetRasterBand(1).GetColorTable()
 
-db = -32
+db = 0
 for i in range(ct.GetCount()):
     (r,g,b,a) = ct.GetColorEntry(i)
-    print db, r, g, b
+    if i == 255:
+        db = None
+    print db, i, r, g, b
     if max([r,g,b]) >= 256 or min([r,g,b]) < 0:
         print 'Invalid Color, assigning black!'
         r,g,b = [0,0,0]
     cursor.execute("""INSERT into iemrasters_lookup(
         iemraster_id, coloridx, value, r, g, b) VALUES (%s,%s,%s,%s,%s,%s)
-        """, (2, i, db, r, g, b))
-    db += 0.5
-    if db > 95:
+        """, (3, i, db, r, g, b))
+    if i == 255:
         break
+    db += 0.1
+    #if db > 95:
+    #    break
 
 cursor.close()
 pgconn.commit()
