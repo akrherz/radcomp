01e85476772009cc2b3339c9fc7cac43eb3f2149
akrherz <akrherz@iastate.edu>
2015-05-08 14:43:19 -0500

    make N0Q ructemps script a bit more robust

--- ructemps.py
+++ ructemps.py
@@ -1,4 +1,6 @@
-# Our hope is to get data valid for the next hour, so we'll try hard to do just that!
+"""Our hope is to get data valid for the next hour, so we'll try hard to do
+just that!
+"""
 import numpy as np
 import pygrib
 import os
@@ -14,18 +16,26 @@ utc = utc + datetime.timedelta(hours=hr)
 utc = utc.replace(tzinfo=pytz.timezone("UTC"))
 
 # Search for valid file
+grbs = None
 for fhour in range(10):
     ts = utc - datetime.timedelta(hours=fhour)
     fstr = "%03i" % (fhour,)
-    fn = ts.strftime("/mesonet/ARCHIVE/data/%Y/%m/%d/model/rap/"+
-                     "%H/rap.t%Hz.awp130f"+fstr+".grib2")
-    if os.path.isfile(fn):
+    fn = ts.strftime(("/mesonet/ARCHIVE/data/%Y/%m/%d/model/rap/"
+                      "%H/rap.t%Hz.awp130f"+fstr+".grib2"))
+    if not os.path.isfile(fn):
+        continue
+
+    try:
+        grib = pygrib.open(fn)
+        grbs = grib.select(name='2 metre temperature')
+    except:
+        print('ructemps.py error with fn: %s' % (fn,))
+        continue
+    if len(grbs) > 0:
         break
 
-grib = pygrib.open(fn)
-grbs = grib.select(name='2 metre temperature')
-if len(grbs) == 0:
-    print 'Could not find 2m Temperature! %s' % (fn,)
+if grbs is None:
+    print("Complete ructemps.py failure for %s" % (utc,))
     sys.exit()
 tmpk_2m = grbs[0].values
 lat, lon = grbs[0].latlons()
@@ -33,11 +43,10 @@ lat, lon = grbs[0].latlons()
 nc = netCDF4.Dataset('data/ructemps.nc', 'a')
 xx, yy = np.meshgrid(nc.variables['lon'][:], nc.variables['lat'][:])
 
-T = interpolate.griddata((lon.ravel(), lat.ravel()), tmpk_2m.ravel(), (xx,yy),
+T = interpolate.griddata((lon.ravel(), lat.ravel()), tmpk_2m.ravel(), (xx, yy),
                          method='cubic')
 
 data = nc.variables['tmpc']
-writehr = utc.hour 
-#print 'Updated RUCTEMPS HR %s' % (writehr,)
-data[writehr,:,:] = T - 273.15
+writehr = utc.hour
+data[writehr, :, :] = T - 273.15
 nc.close()
