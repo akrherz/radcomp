e3b02cceb8250acd3df40a7568fb068a94b7649c
akrherz <akrherz@iastate.edu>
2015-10-12 11:50:56 -0500

    add EET to metadata

--- scripts/add_iemraster_metadata.py
+++ scripts/add_iemraster_metadata.py
@@ -1,32 +1,22 @@
 
 import osgeo.gdal as gdal
 import psycopg2
-pgconn = psycopg2.connect(database='mesosite')
+pgconn = psycopg2.connect(database='mesosite', host='localhost', port=5555,
+                          user='mesonet')
 cursor = pgconn.cursor()
 
-g = gdal.Open('a2m.png')
-
-ct = g.GetRasterBand(1).GetColorTable()
-
-db = 0
-for i in range(ct.GetCount()):
-    (r,g,b,a) = ct.GetColorEntry(i)
-    if i == 255:
-        db = None
-    print db, i, r, g, b
-    if max([r,g,b]) >= 256 or min([r,g,b]) < 0:
-        print 'Invalid Color, assigning black!'
-        r,g,b = [0,0,0]
+feet = 0
+for i, line in enumerate(open("../gempak/tables/luts/iem_eet.tbl")):
+    if i == 0:
+        continue
+    if i == 72:
+        break
+    (r, g, b) = line.split()
     cursor.execute("""INSERT into iemrasters_lookup(
         iemraster_id, coloridx, value, r, g, b) VALUES (%s,%s,%s,%s,%s,%s)
-        """, (3, i, db, r, g, b))
-    if i == 255:
-        break
-    db += 0.1
-    #if db > 95:
-    #    break
+        """, (9, i - 1, feet, r, g, b))
+    feet += 1000
 
 cursor.close()
 pgconn.commit()
 pgconn.close()
-    
