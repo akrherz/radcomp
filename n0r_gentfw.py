daf68e16e80b729f20a902248fd072b0ffcd612a
akrherz <akrherz@iastate.edu>
2019-11-14 21:34:43 -0600

    modernize legacy python n0r script

--- n0r_gentfw.py
+++ n0r_gentfw.py
@@ -1,35 +1,37 @@
-#!/usr/bin/env python
-import mx.DateTime
+"""Generate things."""
+import datetime
 import sys
-import pg
+
+from pyiem.util import get_dbconn
 
 v = sys.argv[1]
-ts = mx.DateTime.strptime(v, "%Y%m%d%H%M")
+ts = datetime.datetime.strptime(v, "%Y%m%d%H%M")
 
-out = open("n0r%s.tfw" % (v,), 'w')
-
-out.write("""0.01
+with open("n0r%s.tfw" % (v,), 'w') as fh:
+    fh.write("""0.01
 0.0
 0.0
 -0.01
 -126.0
 50.0""")
 
-out.close()
-
 if sys.argv[2] != "n0r":
     sys.exit(0)
 
-mydb = pg.connect('postgis', 'iemdb-postgis.local')
-mydb.query("SET TIME ZONE 'GMT'")
+pgconn = get_dbconn('postgis')
+cursor = pgconn.cursor()
 sql = """SELECT * from nexrad_n0r_tindex WHERE datetime = '%s'
-    """ % (ts.strftime("%Y-%m-%d %H:%M"), )
+    """ % (ts.strftime("%Y-%m-%d %H:%M+00"), )
 sql2 = """INSERT into nexrad_n0r_tindex
     (the_geom, datetime, filepath) values
- (GeomFromEWKT('SRID=4326;MULTIPOLYGON(((-126 50,-66 50,-66 24,-126 24,-126 50)))'),
+ (GeomFromEWKT('SRID=4326;
+  MULTIPOLYGON(((-126 50,-66 50,-66 24,-126 24,-126 50)))'),
     '%s', '/mesonet/ARCHIVE/data/%s/GIS/uscomp/n0r_%s.png')
 """ % (ts.strftime("%Y-%m-%d %H:%M"), ts.strftime("%Y/%m/%d"),
        ts.strftime("%Y%m%d%H%M"))
-rs = mydb.query(sql).dictresult()
-if len(rs) == 0:
-    mydb.query(sql2)
+cursor.execute(sql)
+if cursor.rowcount == 0:
+    cursor.execute(sql2)
+
+cursor.close()
+pgconn.commit()
