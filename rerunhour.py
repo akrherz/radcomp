880b0df775c521020068861ef1c93ce3c2c1c5cb
akrherz <akrherz@iastate.edu>
2023-05-25 13:25:55 -0500

    check all the sectors

--- rerunhour.py
+++ rerunhour.py
@@ -29,14 +29,15 @@ def main():
             cmd = f"csh n0r.csh {now:%Y %m %d %H %M} n0r 1"
             subprocess.call(cmd, shell=True)
 
-        uri = now.strftime(
-            "http://mesonet.agron.iastate.edu/archive/data/%Y/%m/%d/"
-            "GIS/uscomp/n0q_%Y%m%d%H%M.png"
-        )
-        req = requests.get(uri, timeout=10)
-        if req.status_code != 200:
-            LOG.info("Reprocess n0q: %s", now)
-            for sector in ["US", "PR", "AK", "HI", "GU"]:
+        # Need to ensure all sectors exist so to keep tilecache happy
+        for sector in ["US", "PR", "AK", "HI", "GU"]:
+            uri = now.strftime(
+                "http://mesonet.agron.iastate.edu/archive/data/%Y/%m/%d/"
+                f"GIS/{sector.lower()}comp/n0q_%Y%m%d%H%M.png"
+            )
+            req = requests.get(uri, timeout=10)
+            if req.status_code != 200:
+                LOG.info("Reprocess n0q: %s sector: %s", now, sector)
                 cmd = f"sh production.sh {sector} {now:%Y %m %d %H %M} OLD"
                 subprocess.call(cmd, shell=True)
 
